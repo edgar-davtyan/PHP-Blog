e3d04dbb49dc4683fedfdbd4d7b1baa995ec0310
Edgar <edgar.davtyan.02@gmail.com>
2020-03-12 13:42:27 +0400

    blogs

---
+++ app/templates/home.php
@@ -0,0 +1,51 @@
+<div class="col-md-8">
+    <?php
+    foreach ($data as $articles) {
+        ?>
+        <article class="post">
+            <div class="post-thumb">
+                <a href="<?= $articles["link"] ?>"><img src="<?=$articles["img"]?>" alt=""></a>
+
+                <a href="<?= $articles["link"] ?>" class="post-thumb-overlay text-center">
+                    <div class="text-uppercase text-center">View Post</div>
+                </a>
+            </div>
+            <div class="post-content">
+                <header class="entry-header text-center text-uppercase">
+                    <h6><a href="#"><?=$articles["category"]?></a></h6>
+
+                    <h1 class="entry-title"><a href="<?= $articles["link"] ?>"><?=$articles["title"]?></a></h1>
+
+
+                </header>
+                <div class="entry-content">
+                    <p><?=$articles["text"]?></p>
+
+                    <div class="btn-continue-reading text-center text-uppercase">
+                        <a href="<?= $articles["link"] ?>" class="more-link">Continue Reading</a>
+                    </div>
+                </div>
+                <div class="social-share">
+                    <span class="social-share-title pull-left text-capitalize">By <a href="#"><?= $articles["author"] ?></a> On <?= $articles["date"] ?> </span>
+                    <ul class="text-center pull-right">
+                        <li><a class="s-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a class="s-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a class="s-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a class="s-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a class="s-instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+
+        <?php
+    }
+    ?>
+    <ul class="pagination">
+        <li class="active"><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#"><i class="fa fa-angle-double-right"></i></a></li>
+    </ul>
+</div>
