35af82a0f4cf3c073fcfecfe55c9a9fa3585ad72
Edgar <edgar.davtyan.02@gmail.com>
2020-07-01 13:55:44 +0400

    add models files

--- app/index.php
+++ app/index.php
@@ -5,13 +5,16 @@ $title = "home";
 if (!empty($_GET) && !empty($_GET["p"])) {
     $page = $_GET["p"];
 }
+require ("controllers/models/Model.php");
 require('templates/header.php');
 if ($page == "") {
     require('templates/home.php');
 } else {
     if (file_exists("templates/$page.php")
     ) {
-        if (file_exists("Controllers/$page-controller.php")){
+        if (file_exists("controllers/models/$page-model.php")){
+            require("controllers/models/$page-model.php");
+        }if (file_exists("Controllers/$page-controller.php")){
             require("Controllers/$page-controller.php");
         }
         require("templates/$page.php");
